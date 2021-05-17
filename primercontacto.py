4e589592189e0523091bb755267769c9522118f2
John <vojohn95@gmail.com>
2021-05-17 15:41:45 -0500

    Terminado cadena de caracteres

--- primercontacto.py
+++ primercontacto.py
@@ -3,3 +3,7 @@ curso = "empece el curso de python"
 print(test + curso)
 #impresion de diagonales y puntos sin perderlos
 print(r'C:\users\clases')
+#impresion multilinea
+print("""De esta manera
+    nos ahorramos a nun
+    pero no a \t una que es para tabulación""")
