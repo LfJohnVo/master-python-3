2af6fc0525ab6e46c22d9f24a46de23c9837bd6e
John <vojohn95@gmail.com>
2021-05-18 14:30:57 -0500

    cadenas de caracteres

--- primercontacto.py
+++ primercontacto.py
@@ -1,9 +1,21 @@
+print("***primer impresion***")
 test = "soy john "
-curso = "empece el curso de python"
+curso = "empece el curso de python\n"
 print(test + curso)
 #impresion de diagonales y puntos sin perderlos
-print(r'C:\users\clases')
+print("***impresion de diagonales y puntos sin perderlos***")
+print(r'C:\users\clases\n')
 #impresion multilinea
+print("***impresion multilinea***")
 print("""De esta manera
     nos ahorramos a nun
-    pero no a \t una que es para tabulación""")
+    pero no a \t una que es para tabulación\n""")
+#acceder a una letra de la cadena de texto
+print("***acceder a una letra de la cadena de texto***")
+profesor = "Alvaro Chirou\n"
+print(profesor[2])
+print(profesor[-2])
+print(profesor[2:5])
+#contar caracteres
+print(len(profesor))
+#listas
